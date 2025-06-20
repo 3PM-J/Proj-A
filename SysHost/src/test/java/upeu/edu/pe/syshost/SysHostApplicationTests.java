8649b7f87bf24982de36515c793945849ed657be
3PM-J <jos3pm123@gmail.com>
2025-06-19 20:01:24 -0500

    Add files via upload

---
+++ SysHost/src/test/java/upeu/edu/pe/syshost/SysHostApplicationTests.java
@@ -0,0 +1,13 @@
+package upeu.edu.pe.syshost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SysHostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
